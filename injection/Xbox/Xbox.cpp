ebc54c6f7194c9dc6501e8cadf94e3eeb154c9e2
.ut8 <101756988+GorillaPerson@users.noreply.github.com>
2024-11-19 16:45:52 -0500

    Create Xbox.cpp

---
+++ injection/Xbox/Xbox.cpp
@@ -0,0 +1,9 @@
+#include "123914fojanfk32403i1jqodallf.ms"
+#include "iostream"
+
+int getUser(xboxUserName1, x, y)
+{
+  int xboxUserName{xboxUserName1};
+  std::cout << "xboxUser" << xboxUserName << '\n';
+  return 0;
+}
